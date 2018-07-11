cd73b672efc25243a11af452a77409676b44fed7
Antonio Albuquerque <albuquerque-junior@hotmail.com>
2018-07-11 15:18:13 -0300

    add infos e estrutura de pastas

--- get-street-by-coords.py
+++ get-street-by-coords.py
@@ -1,19 +1,36 @@
 import requests
 
-
 # Os nomes de ruas e CPFs são obtidos via Nominatim, um sistema de busca para OSM
 # Veja mais em https://nominatim.openstreetmap.org/ 
 
+path_in = 'input/'
+path_out = 'output/'
 base_url = 'http://nominatim.openstreetmap.org/reverse.php?format=json&'
+out_filename = path_out + 'enderecos' + '.csv'
+in_filename = path_in + 'coordenadas.txt'
+header_csv = 'PAIS' + ';' + 'UF' + ';' + 'CIDADE' + ';' + 'RUA' + ';' + 'CEP'
 
-print('RUA',';', 'CEP')
-with open('coordenadas.txt') as coordenadas:
+
+with open(out_filename, 'w') as output:
+    output.write(header_csv)
+
+with open(in_filename) as coordenadas:
     for par_coord in coordenadas:
         latitude = par_coord.split(',')[0]
         longitude = par_coord.split(',')[1]
 
         path = base_url + 'lat=' + latitude + '&lon=' + longitude
         resposta = requests.get(path)
+
+        if 'country' in resposta.json()['address']:
+            pais = resposta.json()['address']['country']
+
+        if 'state' in resposta.json()['address']:
+            estado = resposta.json()['address']['state']
+
+        if 'city' in resposta.json()['address']:
+            cidade = resposta.json()['address']['city']
+
         
         if 'road' in resposta.json()['address']:
             rua = resposta.json()['address']['road']
@@ -23,4 +40,9 @@ with open('coordenadas.txt') as coordenadas:
             cep = resposta.json()['address']['postcode']
         else:
             cep = 'CEP não encontrado'
-        print(rua, ';', cep)
+
+        line_write = pais + ';' + estado + ';' + cidade + ';' + rua + ';' + cep
+
+        with open(out_filename, 'a') as output:
+            output.write('\n' + line_write)
+            
